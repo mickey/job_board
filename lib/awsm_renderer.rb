5b94b68de86ca80770ee802673f6f08bb2819d71
Michael Bensoussan <pada51@gmail.com>
2010-10-31 16:27:39 +0100

    update awsm_renderer

--- lib/awsm_renderer.rb
+++ lib/awsm_renderer.rb
@@ -1,42 +1,25 @@
 class AwsmRenderer < WillPaginate::ViewHelpers::LinkRenderer
 
-  def to_html
-    html = pagination.map do |item|
-      item.is_a?(Fixnum) ?
-        page_number(item) :
-        send(item)
-    end.join(@options[:separator])
-
-    @options[:container] ? html_container(html) : html
+  protected
+  
+  def html_container(html)
+    tag(:div, tag(:ul, html), container_attributes.merge(:class => "pagination"))
   end
-
-  # def to_html
-  #   links = @options[:page_links] ? windowed_links : []
-  # 
-  #   links.unshift(page_link_or_span(@collection.previous_page, 'previous', @options[:previous_label]))
-  #   links.push(page_link_or_span(@collection.next_page, 'next', @options[:next_label]))
-  # 
-  #   html = links.join(@options[:separator]).html_safe
-  #   @options[:container] ? @template.content_tag(:div, content_tag(:ul, html), :class => 'pagination') : html
-  # end
-
-# protected
-# 
-#   def page_link_or_span(page, span_class, text = nil)
-#     text ||= page.to_s
-#     if page && page != current_page
-#       page_link(page, text, :class => span_class)
-#     else
-#       page_span(page, text, :class => span_class)
-#     end
-#   end
-# 
-#   def page_link(page, text, attributes = {})
-#     @template.content_tag(:li, @template.link_to(text, url_for(page)), attributes)
-#   end
-# 
-#   def page_span(page, text, attributes = {})
-#     @template.content_tag(:li, text, attributes)
-#   end
-
+  
+  def page_number(page)
+    unless page == current_page
+      tag(:li, link(page, page, :rel => rel_value(page)))
+    else
+      tag(:li, page)
+    end
+  end
+  
+  def previous_or_next_page(page, text, classname)
+    unless page == current_page
+      tag(:li, link(text, page), :class => classname)
+    else
+      tag(:li, text, :class => classname + ' disabled')
+    end
+  end
+  
 end
